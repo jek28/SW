ebd8d4c8d2bb85823312a0c0e5e06e2c888e84c5
jek28 <jek28@hotmail.it>
2020-02-04 15:31:26 +0100

    Controller, Analisi e Errori perfetti

--- src/app/analyze/analyze.component.ts
+++ src/app/analyze/analyze.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import {Router} from '@angular/router'
 import {Chart} from 'chart.js';
+import 'chartjs-plugin-zoom'
+
 import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -22,9 +24,12 @@ import { Observable } from 'rxjs/Observable';
 export class AnalyzeComponent implements OnInit {
 
   rixfromserver: any ;
+  
   analyzeForm;
   dataforsw= new AnalyzeJSON();
   chart=[];
+  hist=[];
+  cloud=[];
   graphexist=false;
   
   
@@ -48,6 +53,7 @@ export class AnalyzeComponent implements OnInit {
 
       this.sqlservice.GetSettings().subscribe(res=> {  
       this.rixfromserver=res;
+      
       this.analyzeForm = this.formBuilder.group({
       time: this.rixfromserver.analysis[0].time,
       zona1: this.rixfromserver.analysis[0].zona1,
@@ -73,6 +79,8 @@ export class AnalyzeComponent implements OnInit {
     this.graphexist=false;
     console.log("reset grafico ");
     this.chart.destroy();
+    this.hist.destroy();
+    this.cloud.destroy();
     
 
   }
@@ -105,8 +113,124 @@ export class AnalyzeComponent implements OnInit {
           
           const modalRef = this.modalService.open(MymodalComponent);
           modalRef.componentInstance.my_modal_content = 'Grado e punto di settle wave calcolati ';
+          
+          this.createHist(this.rixfromserver.hist_count,this.rixfromserver.hist_division)
+          this.createCloud(this.rixfromserver.DateTimeID,this.rixfromserver.SWDegrees)
+          this.manageGraph()
 
-            if (this.graphexist==false){
+            
+                
+          }
+        
+
+          
+
+    
+      
+   
+    }
+
+    createHist(hist_count,hist_division) {
+
+        if (this.graphexist==true){
+          this.hist.destroy()
+        }
+        this.hist= new Chart('canvas_hist', {
+                type:'bar',
+                data: {
+                labels: hist_division, //none
+                datasets: [    //none 
+                  {
+                  label: 'N. SW detected ',
+                  data: hist_count,//none
+                  
+                  borderColor: "#bae755",
+                  borderDash: [5, 5],
+                  backgroundColor: "#e755ba",
+                  pointBackgroundColor: "#55bae7",
+                  pointBorderColor: "#55bae7",
+                  pointHoverBackgroundColor: "#55bae7",
+                  pointHoverBorderColor: "#55bae7",
+                  fill: false 
+                  },
+                  ]
+                },
+                options: {
+                legend:{
+                display:true
+                },
+                scales:{
+                  xAxes:[{
+                  display:true
+                  }],
+                  yAxes:[{
+                  display:true
+                  }]
+                }
+                }
+                })  
+
+    }
+
+    createCloud(DateTimeID, Degrees) {
+
+        if (this.graphexist==true){
+          this.cloud.destroy()
+        }
+        this.cloud= new Chart('canvas_cloud', {
+                type:'line',
+                data: {
+                labels: DateTimeID, //none
+                datasets: [    //none 
+                  {
+                  label: 'N. SW detected ',
+                  data: Degrees,//none
+                  
+                  borderColor: "#55bae7",
+                  borderDash: [5, 5],
+                  //backgroundColor: "#e755ba",
+                  pointBackgroundColor: "#55bae7",
+                  pointBorderColor: "#55bae7",
+                  pointHoverBackgroundColor: "#55bae7",
+                  pointHoverBorderColor: "#55bae7",
+                  fill: false ,
+                  showLine: false
+                  },
+                  ]
+                },
+                options: {
+                  legend:{
+                  display:true
+                  },
+                  scales:{
+                    xAxes:[{
+                      display:false
+                     }],
+                    yAxes:[{
+                      display:true,
+                      ticks: {
+                        min: -1,
+                        max: 2
+                        }
+                      }]
+                  },
+                  pan: {
+                    enabled: true,
+                    mode: 'y',     
+                    },
+                  zoom: {
+                    enabled: true,         
+                    mode: 'y',     
+                  },
+                  responsive: true
+                  },
+                 
+                })  
+
+    }
+
+    manageGraph(){
+      if (this.graphexist==false){
               
               
               var date = new Date(this.rixfromserver.Datetime * 1000).toISOString() //.match(/(\d{2}:\d{2}:\d{2})/)
@@ -152,18 +276,7 @@ export class AnalyzeComponent implements OnInit {
                   pointHoverBorderColor: "#55bae7",
                   fill: false
                   },
-                  {
-                  label: 'element analyzed',
-                  data: [this.rixfromserver.dataline],//none
-                  borderColor: "#3D1107",
-                  borderDash: [5, 5],
-                  backgroundColor: "#682213",
-                  pointBackgroundColor: "#0000FF",
-                  pointBorderColor: "#665C0D",
-                  pointHoverBackgroundColor: "#55bae7",
-                  pointHoverBorderColor: "#55bae7",
-                  fill: false
-                  },
+                  
                   
                   ]
                 },
@@ -194,7 +307,7 @@ export class AnalyzeComponent implements OnInit {
                 this.chart.data.datasets[0].data.push([this.rixfromserver.SWDegree]);
                 this.chart.data.datasets[1].data.push([this.rixfromserver.SWPoint]);
                 this.chart.data.datasets[2].data.push([this.rixfromserver.missing]);
-                this.chart.data.datasets[3].data.push([this.rixfromserver.dataline]);
+                
                 
                 
 
@@ -204,19 +317,11 @@ export class AnalyzeComponent implements OnInit {
                   this.chart.data.datasets[0].data.shift();
                   this.chart.data.datasets[1].data.shift();
                   this.chart.data.datasets[2].data.shift();
-                  this.chart.data.datasets[3].data.shift();
+                  
                   
                 }
                 this.chart.update();
-                
-          }
-        
-
-          }
-
-    
-      
-   
+              }
     }
 
 
